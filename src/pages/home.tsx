80d56741ea9f86f1643058805ad5cebd95183dd0
EV-OD <evod599@gmail.com>
2024-02-05 21:14:03 +0545

    Remove unused files and update home page animation

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,24 +5,24 @@ import Intro from "./intro";
 import usePageStore from "../store/pageStore";
 
 function Home() {
-  const [opacity, setOpacity] = useState(1);
+  const [opacity, setOpacity] = useState(0);
   const { setInitialAnimationFinished } = useTimingStore();
 
-  // useEffect(() => {
-  //   let id;
-  //   id = setTimeout(() => {
-  //     setOpacity(1);
-  //     setInitialAnimationFinished(true);
-  //   }, 6500);
-  //   return () => clearTimeout(id);
-  // }, []);
+  useEffect(() => {
+    let id;
+    id = setTimeout(() => {
+      setOpacity(1);
+      setInitialAnimationFinished(true);
+    }, 6500);
+    return () => clearTimeout(id);
+  }, []);
 
   return (
     <div className="w-full h-full ">
       <div style={{ opacity: opacity }} className="w-full h-full">
         <Intro />
       </div>
-      {/* <LoveWaiting /> */}
+      <LoveWaiting />
     </div>
   );
 }
